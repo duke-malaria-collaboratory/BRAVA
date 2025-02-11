cd3076de431f4f54b69e09ec3e66d303fb1ab6ed
Rapid User <rapiduser@rapid-2143.vm.duke.edu>
2025-02-11 10:57:53 -0500

    fix create_summaries bugs

--- scripts/step14_create_summaries.R
+++ scripts/step14_create_summaries.R
@@ -11,15 +11,10 @@ long_trim_summary <- read_delim(snakemake@params[["trim_summaries"]],
   mutate(read_ct = as.numeric(read_ct),
          type = ifelse(grepl('Percent', read_type), 'Percent', 'Count'),
          read_type = gsub(' Percent', 's', read_type)) %>% 
-  # filter(name != 'Input Read Pairs') %>% # this is the raw reads
   bind_cols(trim_summary_names %>% slice(rep(1:n(), each = 9))) %>% 
-  # group_by(sample) %>% 
-  filter(type == 'Count') # %>%
-  # select(-type) %>% 
-  # mutate(raw_reads = round(sum(value))) %>% 
-  # pivot_wider(names_from = name, values_from = value)
+  filter(type == 'Count') %>%
+  select(-type) 
 
-long_trim_summary <- select(long_trim_summary, -c("type"))
 print("Long trim summary:")
 print(long_trim_summary)
 
@@ -37,7 +32,7 @@ pre_filt_read_cts <- read_delim(snakemake@params[["pre_filt_fastq_counts"]],
 filt_read_cts <- read_delim(snakemake@params[["filt_fastq_counts"]],
                             col_names = c('path', 'read_ct')) %>%
   mutate(sample = gsub('.*/|_._filt.fastq.gz|_final', '', path),
-         target = gsub(snakemake@params[["all_filtered_files"]], '', path),
+         target = str_match(path, 'haplotype_output/(\\w+?)/bb')[,2],
          read_type = 'filt', 
          .before = 1) %>% 
   select(-path) %>% 
@@ -46,10 +41,7 @@ filt_read_cts <- read_delim(snakemake@params[["filt_fastq_counts"]],
 long_read_cts <- bind_rows(pre_filt_read_cts, filt_read_cts) %>% 
   distinct() %>%
   group_by(sample, target, read_type) %>% 
-  slice_max(read_ct) # %>% 
-  # pivot_wider(names_from = type, values_from = count)
-
-# wide_read_cts <- read_cts %>% pivot_wider(names_from = target, values_from = c(filt, prefilt))
+  slice_max(read_ct)
 
 print(long_read_cts)
 
@@ -57,27 +49,9 @@ print("Long summary:")
 long_summary <- bind_rows(long_trim_summary, long_read_cts)
 print(long_summary, n=Inf)
 print("Wide summary:")
-wide_summary <- long_summary %>% pivot_wider(names_from = c(target, read_type), values_from = read_ct)
+wide_summary <- long_summary %>% pivot_wider(names_from = c(target, read_type), values_from = read_ct) %>%
+  dplyr::rename_with(~str_remove(., "NA_"))
 print(wide_summary, n=Inf)
 
 write_csv(long_summary, snakemake@output[["long_summary"]])
 write_csv(wide_summary, snakemake@output[["wide_summary"]])
-# print(wide_read_cts)
-#write_csv(trim_summary, snakemake@output[["final_summary"]])
-#write_csv(read_cts, snakemake@output[["final_read_counts"]])
-# read_cts <- split(read_cts, read_cts$target)
-
-# for (i in read_cts) {
-#     print(i)
-#     i = select(i %>% ungroup(), -c("target"))
-#     print(i)
-# }
-
-# print(read_cts)
-# print(read_cts[0])
-# ama_read_cts = select(read_cts$ama %>% ungroup(), -c("target"))
-# print(ama_read_cts)
-# ama_read_cts = select(read_cts$ama %>% ungroup(), -c("target"))
-#final_summary <- merge(trim_summary, read_cts, by = "sample")
-#print("final summary")
-#print(final_summary)
